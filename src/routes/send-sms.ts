2a531be7c8f7c5e650be1c8a7a62cdcacd35cb5c
João Moreira <joaojpmoreira25@gmail.com>
2024-05-16 13:02:14 +0000

    feat: funcionarios

--- src/routes/send-sms.ts
+++ src/routes/send-sms.ts
@@ -1,60 +1,69 @@
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { z } from "zod";
-import { FastifyInstance } from "fastify";
-import { BadRequest } from "./_errors/bad-request";
-import dotenv from 'dotenv';
-dotenv.config();
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { FastifyInstance } from 'fastify'
+import { BadRequest } from './_errors/bad-request'
+import dotenv from 'dotenv'
+dotenv.config()
 import pkg from 'twilio'
 const { Twilio } = pkg
 
 const twilio: Twilio = singleton<Twilio>(
-  'twilio',
-  () =>
-    new Twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN),
-);
+	'twilio',
+	() => new Twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN)
+)
 
 export function singleton<Value>(name: string, value: () => Value): Value {
-  const g = global as any;
-  g.__singletons ??= {};
-  g.__singletons[name] ??= value();
-  return g.__singletons[name];
+	const g = global as any
+	g.__singletons ??= {}
+	g.__singletons[name] ??= value()
+	return g.__singletons[name]
 }
 
 export async function sendSms(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().post(
-    "/sms",
-    {
-      schema: {
-        summary: "Send SMS",
-        tags: ["sms"],
-        body: z.object({
-          name: z.string().min(4),
-          contact: z.string().min(4),
-          company: z.string().min(4),
-          instagram: z.string().min(4),
-        }),
-        response: {
-          201: z.object({
-            messageSms: z.string(),
-          }),
-        },
-      },
-    },
-    async (request, reply) => {
-      const { name, contact, company, instagram } = request.body;
+	app.withTypeProvider<ZodTypeProvider>().post(
+		'/sms',
+		{
+			schema: {
+				summary: 'Send SMS',
+				tags: ['sms'],
+				body: z.object({
+					name: z.string().min(4),
+					contact: z.string().min(4),
+					company: z.string().min(4),
+          companySize: z.string().min(4),
+					instagram: z.string().min(4),
+				}),
+				response: {
+					201: z.object({
+						messageSms: z.string(),
+					}),
+				},
+			},
+		},
+		async (request, reply) => {
+			const { name, contact, company, companySize, instagram } = request.body
 
-      const text = "Nome: "+name+", WhatsApp: "+contact+", Empresa: "+company+", Instagram: "+instagram
+			const text =
+				'Nome: ' +
+				name +
+				', WhatsApp: ' +
+				contact +
+				', Empresa: ' +
+				company +
+        ', Funcionarios' +
+        companySize +
+				', Instagram: ' +
+				instagram
 
-      console.log(text)
+			console.log(text)
 
-      twilio.messages
-        .create({
-          body:     text,
-          from: "+14793703859",
-          to: "+5513991027026",
-        })
+			twilio.messages.create({
+				body: text,
+				from: '+14793703859',
+				to: '+5513991027026',
+			})
 
-      return reply.status(201).send({ messageSms: text });
-    }
-  );
+			return reply.status(201).send({ messageSms: text })
+		}
+	)
 }
